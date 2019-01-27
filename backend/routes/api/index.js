b837ec364f9ea1559bcc543ec050bca91efac159
Tim Dolan <timmydolan21@gmail.com>
2019-01-27 16:17:21 -0500

    heroku

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -2,20 +2,20 @@ const router = require("express").Router();
 const gameRoutes = require("./games");
 // const path = require("path");
 // // const router = require("express").Router();
-// const apiRoutes = require("./routes");
-// // import path from "path";
+const apiRoutes = require("./routes");
+// import path from "path";
 
-// // import apiRoutes from "./routes";
+// import apiRoutes from "./routes";
 
 // // API Routes
-// router.use("/api", apiRoutes);
+router.use("/api", apiRoutes);
 
 // // If no API routes are hit, send the React app
-// router.use(function(req, res) {
-//   res.sendFile(path.join(__dirname, "../../public/index.html"));
-// });
+router.use(function(req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 
-// module.exports = router;
+module.exports = router;
 
 // Book routes
 router.use("/games", gameRoutes);
