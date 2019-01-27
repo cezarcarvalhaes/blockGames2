f9474c8e93f8d22d83d87b2ba03b944b04a82458
Tim Dolan <timmydolan21@gmail.com>
2019-01-27 16:40:02 -0500

    d

--- client/src/AppRouter.js
+++ client/src/AppRouter.js
@@ -14,20 +14,20 @@ import {
   Navbar,
   NavItem
 } from "react-materialize";
-import PropTypes from "prop-types";
-import { withStyles } from "@material-ui/core/styles";
-// import Card from "@material-ui/core/Card";
-import CardActionArea from "@material-ui/core/CardActionArea";
-import CardActions from "@material-ui/core/CardActions";
-import CardContent from "@material-ui/core/CardContent";
-import CardMedia from "@material-ui/core/CardMedia";
-// import Button from "@material-ui/core/Button";
-import Typography from "@material-ui/core/Typography";
-import MediaCard from "./components/Card/card";
-import Form from "./components/Form/form";
-import products from "./products.json";
-import Card2 from "./components/Card/card2";
-import API from "./API";
+// import PropTypes from "prop-types";
+// import { withStyles } from "@material-ui/core/styles";
+// // import Card from "@material-ui/core/Card";
+// import CardActionArea from "@material-ui/core/CardActionArea";
+// import CardActions from "@material-ui/core/CardActions";
+// import CardContent from "@material-ui/core/CardContent";
+// import CardMedia from "@material-ui/core/CardMedia";
+// // import Button from "@material-ui/core/Button";
+// import Typography from "@material-ui/core/Typography";
+// import MediaCard from "./components/Card/card";
+// import Form from "./components/Form/form";
+// import products from "./products.json";
+// import Card2 from "./components/Card/card2";
+// import API from "./API";
 import Products from "./components/product";
 
 import Payments from "./components/payments";
--- client/src/index.js
+++ client/src/index.js
@@ -12,4 +12,4 @@ ReactDOM.render(
   </AppRouter>,
   document.getElementById("root")
 );
-registerServiceWorker();
+// registerServiceWorker();
