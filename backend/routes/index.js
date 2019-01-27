679aaec0453102338f7b50277db8ad4ee3e1de1f
Tim Dolan <timmydolan21@gmail.com>
2019-01-27 17:43:00 -0500

    heroku

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -10,10 +10,10 @@ router.get("/test", (req, res)=> {
 })
 router.use("/api", apiRoutes);
 
-// If no API routes are hit, send the React app
-router.use(function(req, res) {
-  res.sendFile(path.join(__dirname, "client/build"));
-});
+// // If no API routes are hit, send the React app
+// router.use(function(req, res) {
+//   res.sendFile(path.join(__dirname, "client/build"));
+// });
 
 module.exports = router;
 
